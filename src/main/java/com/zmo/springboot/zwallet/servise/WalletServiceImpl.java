03f81c9840b441467c78df8350740320d13a780f
MZ <zeytm2.0@gmail.com>
2024-12-19 01:50:51 +0300

    Добавлен redis

--- src/main/java/com/zmo/springboot/zwallet/servise/WalletServiceImpl.java
+++ src/main/java/com/zmo/springboot/zwallet/servise/WalletServiceImpl.java
@@ -8,7 +8,6 @@ import com.zmo.springboot.zwallet.utils.InsufficientFundsException;
 import com.zmo.springboot.zwallet.utils.WalletNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
